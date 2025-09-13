2d10ff8567d6872e22c12f11494edc936b840194
Pierce Issah <PJIceskull2003@gmail.com>
2025-09-12 21:37:49 -0400

    changed Protocol in weather API URL to HTTPS

--- model/model.js
+++ model/model.js
@@ -1,7 +1,7 @@
 const apiKey = "ec6efbb25da2451c8fe152440242309"; // Weather API Key
 
 export function getCurrentWeather() {
-  let currentWeatherURL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Indianapolis&aqi=no`;
+  let currentWeatherURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Indianapolis&aqi=no`;
 
   // Test JSON data
   // Log
